5b7bdf083e5655aaf13c8b219d0f68eb90b84f89
lamia49 <80590112+lamia49@users.noreply.github.com>
2024-02-19 11:44:49 +0300

    Add files via upload

---
+++ quiz1/src/Main.java
@@ -0,0 +1,92 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+     Scanner input=new Scanner(System.in);
+//     System.out.println("Enter numbers: ");
+//
+//        ArrayList<Integer>number=new ArrayList<>();
+//        number.add(input.nextInt());
+//        number.add(input.nextInt());
+//        number.add(input.nextInt());
+//
+//        int largest=0;
+//        for(int n:number){
+//            if (n > largest) {
+//                largest= n;
+//            }
+//        }
+//    System.out.print(largest);
+
+
+
+        //Qustion2
+
+//      System.out.print("Enter String: ");
+//        String s= input.nextLine();
+//        System.out.print("Enter Index");
+//        int n= input.nextInt();
+//        char given= s.charAt(n);
+//     System.out.println("the chracter is "+given);
+
+     ///qustion3
+
+
+//        int number=0;
+//        int temp=0;
+//        do{
+//            System.out.print("Enter number: ");
+//             number= input.nextInt();
+//                temp+=number;
+//                temp++;
+//
+//        }while (number!=-1);
+//
+//
+//        System.out.println("sum of numbers is "+temp);
+
+
+
+
+
+
+
+        //4
+
+    //    ArrayList<Integer>number=new ArrayList<>();
+    //    number.add(10);
+    //    number.add(-21);
+    //    number.add(30);
+     //   number.add(31);
+    //    number.add(-25);
+   //     for (int n:number){
+  //          if(n>0){
+   //             System.out.println(n+" the number is Postive");
+ //           }else if (n<0){
+ //               System.out.println(n+" the number is negative");
+ //           }
+ //       }
+
+
+     //qustion5
+
+//     ArrayList<String> names=new ArrayList<>();
+//     names.add("TUAWIQ");
+//     names.add("JAVA");
+//     names.add("BOOTcAMP");
+//     names.add("STUDENT");
+//     String temp=names.get(0);
+//     for(String s:names){
+//         for (int i=0;i<s.length();i++){
+//             if(s.length()<temp.length()){
+//             temp=s;
+//
+//             }
+//         }
+//     } System.out.println("The shortest word is "+temp);
+
+
+
+    }}
